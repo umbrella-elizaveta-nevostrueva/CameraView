cbfbf76796cf546af5e411906f1dd90c5d614517
Elizaveta Nevostrueva <elizaveta.nevostrueva@umbrella-web.com>
2019-04-03 13:57:17 +0300

    Try to improve compute video sizes

--- cameraview/src/main/java/com/otaliastudios/cameraview/CameraController.java
+++ cameraview/src/main/java/com/otaliastudios/cameraview/CameraController.java
@@ -487,7 +487,10 @@ abstract class CameraController implements
                     SizeSelectors.and(matchRatio),
                     mPictureSizeSelector
             );
-            list = new ArrayList<>(mCameraOptions.getSupportedVideoSizes());
+            List<Size> listPictureSizes = new ArrayList<>(mCameraOptions.getSupportedPictureSizes());
+            if (listPictureSizes != null)
+                list = new ArrayList<>(mCameraOptions.getSupportedVideoSizes()).retainAll(listPictureSizes);
+            else list = new ArrayList<>(mCameraOptions.getSupportedVideoSizes());
         }
 
         Size result = selector.select(list).get(0);
